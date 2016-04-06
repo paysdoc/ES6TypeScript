10df15e8ab431f8f1e2f1a17e74188adcf0ee834
Rachnerd <rachel_149@hotmail.com>
2016-04-06 08:49:55 +0200

    Readme

---
+++ README.md
@@ -0,0 +1,49 @@
+# ES6 + TypeScript Workshop
+###Quintor
+#####Rachèl Heimbach
+This is an Angular 1 project setup for ES6 + TypeScript
+
+##Content
+#####tsconfig.json
+Options for the TypeScript compiler.
+
+#####/app/
+Contains all libs and source files.
+
+#####/dist/ (distribution) 
+Contains all libs and source files compiled and ready for use. (Will be deleted every time gulp builds!!!)
+
+##Install
+Clone repo and execute the following command:
+- npm install
+
+## Tools
+### Gulp
+Gulp provides this project with browser-sync for faster development and makes sure everything in the app folder ends up in the dist folder.
+### Tasks
+##### gulp serve
+Compiles TypeScript, copies all src files to dist, watches for every file change and starts browser-sync.
+
+##### gulp serve-no-ts
+This task ignores TypeScript if you prefer your IDE compiling TypeScript. Gulp will copy the generated js files to the dist folder.
+
+#### Run Gulp
+##### IDE
+If your IDE supports Gulp, you can execute commands from there.
+
+In Webstorm: right click gulpfile.js -> Show Gulp Tasks.
+
+##### Terminal/nodejs/cmd
+Make sure gulp is globally install: 'npm install -g gulp'.
+
+If you don't have gulp installed you can use npm: 'npm run gulp' || 'npm run gulp-no-ts'.
+
+##### Alternative
+If you're having difficulties with gulp you can let your IDE compile the files and serve the app folder in localhost.
+
+## Assignment
+Start migrating the application to TypeScript.
+Tips:
+- There are declaration files included for the Angular API. This means there's a type and autocompletion for
+every Angular method.
+- 
--- app/src/app.ts
+++ app/src/app.ts
@@ -1,64 +1,14 @@
 /// <reference path="../../typings/angularjs/angular.d.ts" />
 /// <reference path="../../typings/angularjs/angular-route.d.ts" />
-angular.module('qApp', ['fakeBackend', 'ngRoute'])
-    .constant('USER_ROLES', {
-        ALL: '*',
-        ADMIN: 'admin',
-        EMPLOYEE: 'employee'
-    })
-    .config(function ($logProvider, $routeProvider, USER_ROLES) {
-        $logProvider.debugEnabled(true);
+angular.module('qApp', ['fakeBackend', 'ngRoute']);
+/**
+    When using classes for Controllers don't forget to add a static $inject variable for dependency injection.
 
-        $routeProvider
-            .when('/', {
-                templateUrl: 'components/home/home.html',
-                controller: 'HomeCtrl',
-                access_levels: [USER_ROLES.ALL],
-                title: 'Home'
-            })
-            .when('/login', {
-                templateUrl: 'components/login/login.html',
-                controller: 'LoginCtrl',
-                controllerAs: '$ctrl',
-                access_levels: [USER_ROLES.ALL],
-                title: 'Login'
-            })
-            .when('/clients', {
-                templateUrl: 'components/client_overview/client_overview.html',
-                controller: 'ClientOverviewCtrl',
-                controllerAs: '$ctrl',
-                access_levels: [USER_ROLES.EMPLOYEE, USER_ROLES.ADMIN],
-                title: 'Clients overview'
-            })
-            .when('/clients/:id', {
-                templateUrl: 'components/client_detail/client_detail.html',
-                controller: 'ClientDetailCtrl',
-                controllerAs: '$ctrl',
-                access_levels: [USER_ROLES.EMPLOYEE, USER_ROLES.ADMIN],
-                title: 'Client detail'
-            })
-            .when('/reset', {
-                controller: function () {
-                    window.localStorage.clear();
-                    location.reload(true);
-                },
-                template: 'reset..',
-                access_levels: [USER_ROLES.ADMIN],
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    })
-    .run(function ($rootScope, $location, UserService) {
-        $rootScope.$on('$routeChangeStart',
-            function (evt, next, curr) {
-                if(!UserService.authenticate(next.access_levels)) {
-                    if (!!UserService.getUser()) {
-                        alert('Not authorized');
-                        $location.path('/');
-                    } else {
-                        $location.path('/');
-                    }
-                }
-            })
-    });
+    export default class HomeCtrl{
+        static $inject:[string] = ['$location', 'UserService'];
+
+        constructor($location:ng.ILocationService, UserService:UserService) {}
+    }
+
+    Use controllerAs syntax for controllers
+ */
